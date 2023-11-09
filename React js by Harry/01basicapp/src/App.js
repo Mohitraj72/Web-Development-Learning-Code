3f64b7f3099564c82c46d85c8745a4ccac63f1da
Mohit Raj <146150589+Mohitraj72@users.noreply.github.com>
2023-11-09 10:42:01 +0530

    update

--- React js by Harry/01basicapp/src/App.js
+++ React js by Harry/01basicapp/src/App.js
@@ -1,40 +1,6 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 
-// function App() {
-//   return (
-//     <>
-//       <nav>
-//         <ol>
-//           <li><a target="-blank" href="https://getbootstrap.com/docs/5.3/forms/overview/?">Home</a></li>
-//           <li><a href="/">Home</a></li>
-//           <li><a href="/">Home</a></li>
-//           <li><a href="/">Home</a></li>
-//         </ol>
-//       </nav>
-
-//       <h2>This is Me </h2>
-//       <div className="App">
-//         <h3>Hello {name}</h3>
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Edit <code>src/App.js</code> and save to reload.
-//           </p>
-//           <a
-//             className="App-link"
-//             href="https://reactjs.org"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Learn React with Mohit
-//           </a>
-//           <p>I am a good by {name}</p>
-//         </header>
-//       </div>
-//     </>
-//   );
-// }
 
 function App() {
   return (
