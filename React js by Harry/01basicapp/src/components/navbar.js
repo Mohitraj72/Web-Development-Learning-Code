6b30052c8b1876a087686416fd885141c10cd985
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2023-11-09 17:23:11 +0530

    update

---
+++ React js by Harry/01basicapp/src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./../index.css"
+
+const Navbar = () => {
+  
+  return <>
+  <nav>
+
+    this is a navbar
+  </nav>
+
+
+
+  </>;
+};
+
+export default Navbar;
