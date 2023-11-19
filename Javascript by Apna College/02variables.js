75df1837c1c2c84044b0fd13942cf98d93c62c4c
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2023-11-19 17:23:30 +0530

    update

---
+++ Javascript by Apna College/02variables.js
@@ -0,0 +1,42 @@
+//!Variables in JS
+// Variables are containers for data
+
+//! Variable RulesApna College
+// Variable names are case sensitive; “a” & “A” is different.
+// Only letters, digits, underscore( _ ) and $ is allowed. (not even space)
+// Only a letter, underscore( _ ) or $ should be 1st character.
+// Reserved words cannot be variable names
+
+//! let, const & varApna College
+//* var : Variable can be re-declared & updated. A global scope variable.
+//* let : Variable cannot be re-declared but can be updated. A block scope variable.
+//* const : Variable cannot be re-declared or updated. A block scope variable.
+
+//? Here is all of the Variables Values data 
+
+/* age = 24;
+name = "Mohit Raj"
+console.log(name, age + 2); 
+
+x = null;
+y = undefined;
+console.log(x);
+console.log(y);
+
+name = "shyam kumar"
+console.log(name); */
+
+
+//? Here is all of the Let Values data 
+
+// let Name = raj;
+ let age = 44;
+// let marks = 450;
+
+// console.log(Name);
+
+//? 
+
+typeof age;
+
+
