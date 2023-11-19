b6dde363e4d734afa18b0849eb54a0fd0dc73740
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2023-11-19 19:54:36 +0530

    update

---
+++ Javascript_by_Hitesh_chaudhary/01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "Mohit@gmail.com"
+var accountPassword = "1234"
+accountCity = "Bihar"
+
+/* 
+accountId = 2 not allowed
+*/
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/* Prefer not to use var because of issue in block scope and functional acope */
+
+const.table([accountId, accountEmail, accountPassword, accountCity, accountstate])
+
---
+++ Javascript_by_Hitesh_chaudhary/01_basics/03_conversionOperation.js
@@ -0,0 +1,73 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; False =>0
+
+let isLoggedIn = "Mohit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => False
+// "" => False
+// "Mohit" => true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//************ Operations **************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue); 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "Mohit"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2+2 
+
+let GameCounter = 100
+GameCounter++;
+console.log(GameCounter);
+
+
+
+
+
+
+
+
+
