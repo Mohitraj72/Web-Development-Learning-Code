ec3f2dd38f467266f914b7cd0e1fa4487be6adb3
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2023-11-28 13:33:16 +0530

    update

---
+++ Javascript by Apna College/02-video/script.js
@@ -0,0 +1,81 @@
+//! Operators and conditional Statement 
+
+// This Comments print hello World
+//console.log("Hello world");
+
+//* Operators in Js (Arithmetic Operators)
+
+// let a = 5;
+// let b = 2;
+
+
+// console.log("a+b =", a + b );
+// console.log("a-b =", a - b );
+// console.log("a*b =", a * b );
+// console.log("a/b =", a / b );
+// console.log("a**b =", a ** b );
+// console.log("a%b =", a % b ); // 5^2 = 25 
+
+//* Unary Operator 
+// a++; // => a = a + 1 
+// console.log(a); 
+
+// a--; // => a = a - 1 
+// console.log(a);
+
+//! Assignment Opertors
+
+// let a = 5;
+// let b = 2;
+
+// a += 4; // a = a + 4
+// console.log("a = ", a);  //9
+
+// a -= 4; // a = a- 4
+// console.log("a = ", a);  // 5
+
+// a *= 4; // a = a * 4
+// console.log("a = ", a);  // 20
+
+// a /= 4; // a = a / 4
+// console.log("a = ", a);   // 5
+
+// a %= 4; // a = a % 4
+// console.log("a = ", a); // 1
+
+// a **= 4; // a = a ** 4
+// console.log("a = ", a);   // 1
+
+//! Operators in Js
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a == b", a == b); // false
+// console.log("a === b", a === b); // false
+// console.log("a != b", a != b); // true
+// console.log("a !== b", a !== b); // true
+
+
+// console.log("a > b", a > b); // true
+// console.log("a >= b", a >= b); // true
+// console.log("a < b", a < b); // false
+// console.log("a <= b", a <= b); // false
+
+
+//! Logical Operators 
+
+let a = 5;
+let b = 2;
+
+let cond1 = a > b; // true
+let cond2 = a === b; // true
+console.log("cond1 && cond2 = ", cond1 && cond2);
+
+
+
+
+
+
+
+
