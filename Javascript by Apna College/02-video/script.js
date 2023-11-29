52d83d3b813dd485c00dd52b78f6da900ac69479
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2023-11-29 08:38:29 +0530

    update

--- Javascript by Apna College/02-video/script.js
+++ Javascript by Apna College/02-video/script.js
@@ -65,14 +65,65 @@
 
 //! Logical Operators 
 
-let a = 5;
-let b = 2;
+// let a = 5;
+// let b = 2;
 
-let cond1 = a > b; // true
-let cond2 = a === b; // true
-console.log("cond1 && cond2 = ", cond1 && cond2);
+// let cond1 = a > b; // true
+// let cond2 = a === b; // true
+// console.log("cond1 && cond2 = ", cond1 && cond2);  // false 
+
+// console.log("cond1 || cond2 =", a < b || a ===5); // True 
+
+// console.log("cond1 || cond2 =", !(a===6) );   // true 
 
 
+//! Conditional Statements 
+  //* If Statement 
+
+// let age = 65;
+
+// if(age>= 18){
+//   console.log("you an vote me ");
+// }
+
+// if (age<18){
+//   console.log("You can't vote");
+// }
+
+// let mode = "light";
+// let color;
+
+// if(mode === "dark"){
+//   color = "black";
+// }
+
+// if(mode === "light"){
+//   color = "white"
+// }
+
+// console.log(color);
+
+//! If-else statement 
+
+let mode = "light"
+let color;
+if(mode === "dark"){
+  color = "black";
+} else {
+  color = "white";
+}
+  console.log(color);
+
+
+  //* 2nd Example 
+
+let age = 16;
+
+if(age>= 18){
+  console.log("vote");
+} else {
+  console.log("not vote ");
+}
 
 
 
