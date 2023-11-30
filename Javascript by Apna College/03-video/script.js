ad874202d35f3afb24430b0f27efe07ecb3b96cd
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2023-11-30 07:04:34 +0530

    update

---
+++ Javascript by Apna College/03-video/script.js
@@ -0,0 +1,34 @@
+//! Loops In JS 
+
+
+// for (let count = 1; count <=5; count++) {
+//   console.log("Apna College ");
+// }
+
+//* Calculate sum of 1 to 5
+
+// let sum = 0;
+// for (let i = 1; i <= 5 ; i++)
+// {
+//   sum = sum + i;
+// }
+
+// console.log(sum);
+
+
+//! Infinite Loops 
+
+//* Infinite loops : A Loop that never ends 
+
+//for (let i = 1; i >= 0; i++){
+// console.log("i", i ); }
+
+//! While Loop 
+
+// let i = 1 ;
+// while (i <= 10 ){
+//   console.log("Mohit Raj ", i ); i++;
+// }
+
+//! Do-While Loop
+
