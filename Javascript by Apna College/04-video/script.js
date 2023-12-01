7452dd28b78ecab965dd5862ff745c7b140ea6c4
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2023-12-01 06:50:06 +0530

    update

---
+++ Javascript by Apna College/04-video/script.js
@@ -0,0 +1,45 @@
+//! Arrays in JS 
+
+// let array = ["mohit", "nikhil", "lalu", "kajal"]
+
+// console.log(array);
+
+
+// let marks = [78, 45, 82, 475, 782]
+
+// console.log(marks.length);
+// console.log(typeof array);
+// console.log(marks[4]);
+
+//! Loops over an Array 
+
+//* for Loops 
+// for (let i = 0; i < array.length; i++){
+//   console.log(array[i]);
+// }
+
+//* For Of 
+// for (let arra of array){
+//   console.log(array);
+// }
+
+//! Q1 Practice 
+
+// let marks = [75 ,78, 75, 91, 36, 79, 96];
+
+// let sum = 0;
+
+// for (let val of marks){
+//   sum += val;
+// }
+
+// let avg = sum / marks.length;
+// console.log(`avg marks of the class = ${avg}`);
+
+//! Q2 Practice 
+
+
+
+
+
+ 
