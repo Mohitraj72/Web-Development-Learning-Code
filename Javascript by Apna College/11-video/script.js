b8e80108bc0ee10859cfbd35f5ec70b9ca042ff5
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-01-11 15:54:48 +0530

    update

--- Javascript by Apna College/11-video/script.js
+++ Javascript by Apna College/11-video/script.js
@@ -1,7 +1,26 @@
-const student = {
-  fullName: "Mohit Raj",
-  marks: 89.8,
-  printMarks: function () {
-    console.log("marks", this.marks);
-  },
-}
+// const student = {
+//   fullName: "Mohit Raj",
+//   marks: 95.8,
+//   printMarks: function () {
+//     console.log("marks = ", this.marks);  // Student.marks 
+//   },
+// };
+
+// const employee = {
+//   calcTax(){
+//     console.log("Tax rate is 10%");
+//   }
+// }
+
+// const Karan = {
+//   salary: 50000,
+//   calcTax(){
+//     console.log("Tax rate is 20%");
+//   },
+// };
+
+// Karan.__proto__= employee;
+
+let arr = ["Harry", "Mohit"];
+
+console.table(arr);
