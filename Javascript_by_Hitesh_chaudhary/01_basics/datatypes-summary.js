9816b94a7c23e75fd1aec98204fd0e4b65003813
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-04-27 13:46:49 +0530

    update

--- Javascript_by_Hitesh_chaudhary/01_basics/datatypes-summary.js
+++ Javascript_by_Hitesh_chaudhary/01_basics/datatypes-summary.js
@@ -1,3 +1,41 @@
 // Primitive 
 
 // 7 Types : String , Number , Boolean , Null , Undefined , Symbol , BigInt
+
+const score = 100;
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null 
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 1234567890123456n
+
+
+
+
+
+
+// Referance (Non Prmitive)
+
+// Array,  Objects , Functions 
+
+const heros = ["shaktiman", "lala", "Mahatma"];
+let myObj= {
+    name: "Mohit",
+    age: 22,
+
+}
+
+const myFunction = function() {
+    console.log("Hello");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof anotherId);
