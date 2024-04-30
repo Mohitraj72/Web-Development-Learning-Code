23bea63df8853f0b318c28b4b1c185faa541a368
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-04-30 11:11:48 +0530

    update

---
+++ Javascript by Apna College/08-video/script.js
@@ -0,0 +1,16 @@
+let btn1 = document.querySelector("#btn1");
+// btn1.onclick = () => {
+//   console.log("btn1 Was clicked ");
+// };
+
+// let div = document.querySelector("div");
+// div.onmouseover = () => {
+//   console.log("You are inside Div");
+// };
+
+btn1.addEventListener("click", () => {
+    console.log("Button Was Clicked");
+});
+
+
+
--- Javascript_by_Hitesh_chaudhary/01_basics/05_strings.js
+++ Javascript_by_Hitesh_chaudhary/01_basics/05_strings.js
@@ -8,3 +8,7 @@ const gameName = new String('Mohit')
 
 console.log(gameName[0]);
 console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
