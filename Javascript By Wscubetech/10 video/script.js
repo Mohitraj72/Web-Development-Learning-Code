2218c23d70f560807797dcfdec33744da2fa4c13
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-05-03 10:49:44 +0530

    update

---
+++ Javascript By Wscubetech/10 video/script.js
@@ -0,0 +1,12 @@
+//! Array in Javascript 
+// var cars = []; 
+// cars = ["car1", "car2", "car3", "car4"];
+// console.table(cars);
+
+//! Loop in Javascript 
+// for (var count = 0; count <= 10; count++) {
+//     console.log(count);
+// }
+
+//! While Loop in JS 
+
