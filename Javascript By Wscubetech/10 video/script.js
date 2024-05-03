b4c7693b5c42de900010700386f94ebc4884dca9
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-05-03 20:15:24 +0530

    update

--- Javascript By Wscubetech/10 video/script.js
+++ Javascript By Wscubetech/10 video/script.js
@@ -9,4 +9,28 @@
 // }
 
 //! While Loop in JS 
+// var i =0;
+// while (i<10){
+//     console.log("Mohit", i);
+//     i++;
+// }
+
+//! For Each Loop in JS 
+// var cars = ["car1", "car2", "car3", "car4", "car5", "car6"];
+// cars.forEach(car);
+
+// function car(item,index) {
+//     console.table(index +"="+ item);
+// }
+
+//! Continue Statements in Js
+// for (var i = 0; i < 10; i++){
+//     if(i%2 !=0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// Break Statement in Js
+
 
