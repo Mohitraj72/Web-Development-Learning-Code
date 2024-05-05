693e004210afb9d25e6cc8c027d876acb67987bc
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-05-05 11:26:47 +0530

    update

---
+++ Javascript By Wscubetech/15 video/script.js
@@ -0,0 +1,18 @@
+//! Modify Css Using JavaScript 
+
+function modify() {
+    document.getElementById('main').style.fontSize = "100px";
+    document.getElementById('main').style.color = "blue";
+    document.getElementById('main').style.backgroundColor = "yellow";
+}
+
+
+function unmodify() {
+    document.getElementById('main').style.fontSize = "50px";
+    document.getElementById('main').style.color = "red";
+    document.getElementById('main').style.backgroundColor = "aqua";
+}
+
+//! clear timeout Method 
+
+
