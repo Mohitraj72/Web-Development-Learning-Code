d5791aafd89d7a750d40de80af258f24a6a1bce3
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-05-07 10:44:09 +0530

    update

--- Javascript By Wscubetech/18 video/script.js
+++ Javascript By Wscubetech/18 video/script.js
@@ -1,12 +1,27 @@
 //! Printing data using window.print
 
-function printpage(){
+function printpage() {
     // window.print();
-   var body = document.getElementById('body').innerHTML;
-   var data = document.getElementById('data').innerHTML;
-   document.getElementById('body').innerHTML = data;
-   window.print();
-   document.getElementById('body').innerHTML = body;
+    var body = document.getElementById('body').innerHTML;
+    var data = document.getElementById('data').innerHTML;
+    document.getElementById('body').innerHTML = data;
+    window.print();
+    document.getElementById('body').innerHTML = body;
 };
 
 //! Validate number using JS 👇
+function val(elem) {
+    // document.getElementById('res').innerText = elem.value;
+    if (isNaN(elem.value)) {
+        document.getElementById('res').innerText = "Please Enter No Only";
+    } else {
+        document.getElementById('res').innerText = "";
+        if (elem.value.length >= 10) {
+            document.getElementById('res').innerText = "Please enter only 10 digits";
+        } else {
+            document.getElementById('res').innerText = " ";
+        }
+    }
+}
+
+document.getElementById("res").style.color = "red";
