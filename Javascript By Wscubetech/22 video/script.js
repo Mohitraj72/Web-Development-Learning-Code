d1d6446d593a4af0771c15edac42c0ace7477ce5
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-05-11 07:54:44 +0530

    update

---
+++ Javascript By Wscubetech/22 video/script.js
@@ -0,0 +1,38 @@
+//! Exception Handling in JavaScript ⚡
+
+/* function test (){
+    try{
+        // alerth("hello");
+        var number = "Mohit Raj";
+        var x = number.toUpperCase();
+        alert(x);
+        
+
+
+    } catch(err){
+        document.getElementById('res').innerText = err;
+    }
+}  */
+
+// function test() {
+//     try {
+//       const number = "Mohit Raj";
+//       const x = number.toUpperCase();
+  
+//       const resElement = document.getElementById('res');
+//       if (resElement) {
+//         resElement.textContent = x;
+//       } else {
+//         console.error("Element with id 'res' not found");
+//       }
+//     } catch (err) {
+//       console.error(err);
+//       const resElement = document.getElementById('res');
+//       if (resElement) {
+//         resElement.textContent = err.message;
+//       }
+//     }
+//   }
+
+//! Javascript try catch throw statement 
+
