911a2dcd9b74cceac91c76aa4b7e505ad896fee2
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-05-11 11:57:35 +0530

    update

--- Javascript_by_Hitesh_chaudhary/01_basics/05_strings.js
+++ Javascript_by_Hitesh_chaudhary/01_basics/05_strings.js
@@ -2,13 +2,32 @@ const name = "Mohit"
 const repocount = 10
 
 // console.log(name + repocount + " Value");
-console.log(`Hello My name is ${name} and my repo count is ${repocount}`); 
+console.log(`Hello My name is ${name} and my repo count is ${repocount}`);
 
 const gameName = new String('Mohit')
 
 console.log(gameName[0]);
 console.log(gameName.__proto__);
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
 console.log(gameName.charAt(3));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-2, 4);
+console.log(anotherString);
+
+const newStringOne = "   Mohit Raj ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://Mohitraj/subscribe%20newmember";
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
