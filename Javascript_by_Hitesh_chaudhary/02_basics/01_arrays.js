f9950e30e1c3ca5dca610811f2127e7ddb9d60fb
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-05-11 17:24:12 +0530

    update

---
+++ Javascript_by_Hitesh_chaudhary/02_basics/01_arrays.js
@@ -0,0 +1,19 @@
+// Array in JS 
+
+const myArr = [0,1,2,1,4,5,6,7];
+const myHeroes = ["shaktiman", "balvir"];
+
+const mtarr2 = new Array(1,2,3,4,5)
+console.table( myArr[1]);
+
+
+//! Array methods 
+
+myArr.push(6)
+myArr.push(7)
+console.table( myArr);
+
+
+
+
+
