c30106210a74fa82c9aba631123444fc1f638ebf
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-05-20 13:13:32 +0530

    update

--- Javascript_by_Hitesh_chaudhary/04_control_flow/one.js
+++ Javascript_by_Hitesh_chaudhary/04_control_flow/one.js
@@ -1,12 +1,47 @@
 // if
-const isUserloggedIn = true;
-const temperature = 41;
+// const isUserloggedIn = true;
+// const temperature = 41;
 
-if ( temperature === 40 ){
-    console.log("less than 50");
-} else {
-    console.log("temperature is greater than 50");
-}; 
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }; 
 
-console.log("Execute");
+// console.log("Execute");
 // <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200;
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+
+// console.log(`user power: ${power}`);
+
+const balance = 1000;
+
+// if (balance > 500) console.log("test"), console.log("Test 2");  // dont like this 
+
+// if (balance < 500) {
+//     console.log("less Than");
+// } else if (balance < 750) {
+//     console.log("Less Than 750");
+// } else if (balance < 900) {
+//     console.log("Less Than 750");
+// } else {
+//     console.log("Less Than 1200");
+// }
+
+const userLoggedIn = true ;
+const debitcard = true;
+const loggedfromgoogle = false;
+const loggedfromEmail = true;
+
+if (userLoggedIn && debitcard && 2==3){
+    console.log("Allow to Shop ");
+}
+
+if (loggedfromgoogle || loggedfromEmail) {
+    console.log("Allow ");
+}
