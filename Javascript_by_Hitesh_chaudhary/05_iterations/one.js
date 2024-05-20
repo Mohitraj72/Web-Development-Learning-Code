23f92a64f54bf059ac352c800db298cc4372b452
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-05-20 20:57:20 +0530

    update

--- Javascript_by_Hitesh_chaudhary/05_iterations/one.js
+++ Javascript_by_Hitesh_chaudhary/05_iterations/one.js
@@ -1,8 +1,8 @@
 //! For 
 
-for (let index = 0; index < 10; index++) {
-    const element = index;
+for (let i = 0; i <= 10; i++) {
+    const element = i;
     console.log(element);
     
 }
-
+console.log(element);
