ece8ee9fc4d80ff7f6464da581536fe2b4a5b07a
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-05-24 08:48:18 +0530

    update

--- chai aur Django/Mohitraj/Mohitraj/views.py
+++ chai aur Django/Mohitraj/Mohitraj/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, World. you are at Home Now Page ")
+    # return HttpResponse("Hello, World. you are at Home Now Page ")
+    return render(request, 'website/index.html')
+
 
 def about(request):
     return HttpResponse("Hello, World. you are at about Page ")
