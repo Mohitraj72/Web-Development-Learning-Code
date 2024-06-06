c9dde1f997ca9e4582b242c33c92fb2bf683c2b2
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-06-06 10:43:48 +0530

    update

---
+++ chai aur react/01viteproject/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./chai"
+
+function App() {
+  return (
+    <>
+      {/* <h1>Hello World Mohit  </h1> */}
+      <Chai/>
+      <h3> Hello Mohit </h3>
+
+
+
+    </>
+  )
+}
+
+export default App
