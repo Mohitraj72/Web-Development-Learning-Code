4a704fe67d2a2c90b6c56a1dccbe3794879b1e3f
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-08-12 07:20:29 +0530

    'update'

---
+++ 0 to Pro Web dev/Day 18/script.js
@@ -0,0 +1,27 @@
+//! Js object literal 
+
+let student ={
+    name: "Mohit Raj",
+    age:15,
+    marks: 99
+};
+
+const students ={
+    name: "Mohit Raj",
+    age:15,
+    marks: 99
+};
+
+console.log(student);
+
+//! Creating Post for Social media (OBJECTS)
+
+const post = {
+    username: "@mohitraj",
+    content: "This is my #first post",
+    likes: 10,
+    Report: 2,
+    tags: ["@college", "@course"]
+};
+
+console.table(post);
