8fe604cd70585ffdf6c896e44c400d1451466df5
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-12-22 11:09:18 +0530

    'update'

---
+++ 0 to Pro Web dev/Day - 12/script1.js
@@ -0,0 +1,24 @@
+var a = 12;
+var b = 22;
+
+console.log("hey");
+
+// Browser have 3 Features Only 
+
+// console.log();
+console.log("Hello I am Here");
+
+
+// console.warn();
+console.warn("Hello");
+
+
+// console.error();
+console.error("Hello");
+
+
+//console alert 
+alert('msg');
+
+// console Prompt 
+prompt("Enter Your Name");
