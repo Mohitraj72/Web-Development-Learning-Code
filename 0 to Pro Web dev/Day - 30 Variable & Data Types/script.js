24a08d892b9924820287938d118799f03d180659
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-02-14 11:05:02 +0530

    'update'

--- 0 to Pro Web dev/Day - 30 Variable & Data Types/script.js
+++ 0 to Pro Web dev/Day - 30 Variable & Data Types/script.js
@@ -12,3 +12,27 @@
 // console.log(age);
 // solve();
 
+// {
+//     let a = 10;
+//     console.log(a);
+// }
+
+// let a = 20;
+// a = 30;
+
+
+// let a =10;    it can be changed
+// a = "Mohit";
+// a = true;
+// a = null;
+
+
+// const a = 28;   it can't be changed
+// console.log(a);
+
+
+
+
+
+
+
