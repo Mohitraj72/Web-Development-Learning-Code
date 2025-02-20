3786af22307c27c1b806984151f962cfc0187142
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-02-20 15:31:43 +0530

    'update'

--- 0 to Pro Web dev/Day - 31/script.js
+++ 0 to Pro Web dev/Day - 31/script.js
@@ -3,11 +3,11 @@
 let a = 10;
 let b = 5;
 
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a**b);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** b);
 
 // Pre-increment/decrement
 
