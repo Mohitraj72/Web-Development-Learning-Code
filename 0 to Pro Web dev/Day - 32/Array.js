d2e940f4e0c15ed4da8b8d57399572358c0ced53
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-02-21 18:27:51 +0530

    'update'

--- 0 to Pro Web dev/Day - 32/Array.js
+++ 0 to Pro Web dev/Day - 32/Array.js
@@ -5,3 +5,22 @@ let student1 = ["aman","Raj","Baba"];
 console.log(student1);
 
 console.log(student1[1]);
+
+//! Creating Array 
+
+let info = ["Raj", 25, 2555.25];
+
+console.log(info);
+
+
+//! Arrays are Mutable 
+
+let fruit = ["Mango","apple","Litchi"];
+console.log(fruit);
+
+fruit[0] = "Rasgulla";
+fruit[2] = "Pineapple";
+fruit[10] = "Website";
+
+console.table(fruit);
+
