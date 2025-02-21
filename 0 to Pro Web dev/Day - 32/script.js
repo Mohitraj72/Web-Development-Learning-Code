0697c04f3d37d2ced40041e3356b431150469032
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-02-21 08:54:14 +0530

    'update'

---
+++ 0 to Pro Web dev/Day - 32/script.js
@@ -0,0 +1,12 @@
+// String Method - actions that can be performed on objects.
+
+//! 1. Trim Method
+
+let msg = '    hello    ';
+console.log(msg.trim());
+
+//! Uppercase & Lowercase 
+
+let names = 'mohit Raj';
+console.log(names.toUpperCase());
+console.log(names.toLowerCase());
