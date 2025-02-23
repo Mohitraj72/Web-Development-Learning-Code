cc7a250c5c0f0aebce2691e56de2a3364c5305c2
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-02-23 19:20:05 +0530

    'update'

--- 0 to Pro Web dev/Day - 33/script.js
+++ 0 to Pro Web dev/Day - 33/script.js
@@ -1,29 +1,50 @@
 //! Loops In Javascript 
 
-for (let i = 1; i<=5; i++){
+for (let i = 1; i <= 5; i++) {
     // console.log(i);
 }
 
 
 //! Print All Odd Number 
 
-for (let i =1; i<=100; i=i+2){
+for (let i = 1; i <= 100; i = i + 2) {
     // console.log(i);
 }
 
 
-for (let i =15; i>=1; i=i-2){
+for (let i = 15; i >= 1; i = i - 2) {
     // console.log(i);
 }
 
 //! Print all even Number 
 
-for (let i =2 ; i<=10; i= i+2){
+for (let i = 2; i <= 10; i = i + 2) {
     // console.log(i);
 }
 
 
-for (let i = 10; i>=2; i =i-2){
+for (let i = 10; i >= 2; i = i - 2) {
     // console.log(i);
 }
 
+//! Infinite Loops 
+
+for (let i = 1; i >= 0; i++) {
+    // console.log(i);
+}
+
+//! Print the multiplication table for 5 
+
+for (let i = 5; i <= 50; i = i + 5) {
+    console.log(i);
+}
+
+
+//! Nested For loops 
+
+for (let i = 1; i <= 3; i++) {
+    
+    for (let j = 1; j <= 3; j++) {
+        // console.log(j);
+    }
+}
