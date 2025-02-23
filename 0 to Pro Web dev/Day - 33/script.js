43f4267adb9cc69cf3872580ef0cfbb322d3a9af
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-02-23 09:42:23 +0530

    'update'

--- 0 to Pro Web dev/Day - 33/script.js
+++ 0 to Pro Web dev/Day - 33/script.js
@@ -1,4 +1,19 @@
-hhhh
+//! Loops In Javascript 
+
+for (let i = 1; i<=5; i++){
+    // console.log(i);
+}
 
 
-ffff
+//! Print All Odd Number 
+
+for (let i =1; i<=100; i=i+2){
+    // console.log(i);
+}
+
+
+for (let i =15; i>=1; i=i-2){
+    console.log(i);
+}
+
+//! Print all even Number 
