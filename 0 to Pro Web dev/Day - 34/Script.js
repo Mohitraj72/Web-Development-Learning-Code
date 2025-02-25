8b745026e9fd24dc8c90743fd60372eb30667253
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-02-25 09:29:59 +0530

    'update'

---
+++ 0 to Pro Web dev/Day - 34/Script.js
@@ -0,0 +1,37 @@
+//! JS Object Literal 
+
+let delhi = {
+    latitude: "28.7041",
+    longitude: "77.1025"
+};
+
+let students = {
+    age: 23,
+    name: "mohit",
+    Marks: 94.4
+};
+
+console.log(students);
+
+
+//! Create Thread / Twitter Post 
+
+const post = {
+    username: "@Mohitraj",
+    Content: "This is My new Post",
+    likes: 151,
+    reposts: 5,
+    tags: ["@college", "Raj"]
+};
+
+console.log(post["tags"]);
+
+console.log(post.Content);
+
+console.log(post.likes);
+console.log(post.tags[0]);
+
+
+
+//! Conversion in Get Value
+
