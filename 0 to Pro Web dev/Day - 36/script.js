6fa0ae320b134d5d929777dc0dd4997354598dee
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-02-28 19:03:44 +0530

    'update'

---
+++ 0 to Pro Web dev/Day - 36/script.js
@@ -0,0 +1,21 @@
+//! This keywords in javascript 
+
+const students = {
+    name: "Mohit",
+    age: 23,
+    eng: 25,
+    hindi: 88,
+    math: 99,
+    phy: 77,
+    getAvg() {
+        console.log(this);
+        let avg = (this.eng + this.math + this.hindi) /3;
+        console.log(`${this.name} got avg marks = ${avg}`);
+    }
+}
+
+students.getAvg();
+
+
+//! Try and catch 
+
