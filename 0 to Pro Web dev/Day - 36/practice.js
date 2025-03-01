ec679972a45ebaccd6bf1477320ae49efb67e2c2
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-03-01 11:05:53 +0530

    update

--- 0 to Pro Web dev/Day - 36/practice.js
+++ 0 to Pro Web dev/Day - 36/practice.js
@@ -1,2 +1,23 @@
 //! Practice 
 
+// Write an arrow function that returns the square of a number 'n
+
+const sqaure = (n) => n*n;
+
+console.log(sqaure(5));
+
+
+
+//write a functiomn that prints "hello world" 5 times at interval of 2s each.
+
+let id = setInterval (() => {
+    console.log("Hello world");
+
+},2000);
+
+
+setTimeout(() => {
+    clearInterval(id);
+    console.log("clear Interval");
+},10000);
+
