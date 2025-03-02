9c799a2f485482eb087dbd07c765002c33b993be
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-03-02 14:39:06 +0530

    'update'

--- 0 to Pro Web dev/Day - 37/script.js
+++ 0 to Pro Web dev/Day - 37/script.js
@@ -1,9 +1,31 @@
 //! Array Method 
 
-let arr = [1,2,3,4,5,6];
+// let arr = [1, 2, 3, 4, 5, 6];
 
-let print = function (el) {
-    console.log(el);
-};
+// arr.forEach((a) => {
+//     console.log(a);
+// });
 
-arr.forEach(print);
+// arr.forEach(function (a) {
+//     console.log(a);
+// });
+
+// let print = function (el) {
+//     console.log(el);
+// };
+
+// arr.forEach(print);
+
+let array = [
+    { name: "Raj", marks: 92 },
+    { name: "Shani", marks: 88 },
+    { name: "SK", marks: 85 }
+]; 
+
+array.forEach((student) => {
+    console.log(student.marks);
+});
+
+
+//! Map 
+
