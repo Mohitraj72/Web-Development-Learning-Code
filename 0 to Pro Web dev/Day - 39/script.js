d4dbac2fa4181b010b3bc9bfbda06f6d58606e38
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-03-17 20:13:00 +0530

    'update'

--- 0 to Pro Web dev/Day - 39/script.js
+++ 0 to Pro Web dev/Day - 39/script.js
@@ -1,7 +1,14 @@
 let btn = document.querySelector("button");
 console.dir(btn);
 
-btn.onclick = function () {
-    console.log("Hello\n");
-    // alert('msg');
-};
+// btn.onclick = function () {
+//     console.log("Hello\n");
+//     alert('msg');
+// };
+
+
+function sayHello() {
+    alert('msg');
+}
+
+btn.onclick = sayHello;
