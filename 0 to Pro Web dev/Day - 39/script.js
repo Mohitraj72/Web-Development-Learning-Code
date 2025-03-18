380063ed97fde60f53d3afbfccf0800c59c60399
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-03-18 20:46:56 +0530

    'update'

--- 0 to Pro Web dev/Day - 39/script.js
+++ 0 to Pro Web dev/Day - 39/script.js
@@ -1,20 +1,20 @@
-let btn = document.querySelectorAll("button");
-console.dir(btn);
+// let btn = document.querySelectorAll("button");
+// console.dir(btn);
 
 // btn.onclick = function () {
 //     console.log("Hello\n");
 //     alert('msg');
 // };
 
-for (btn of btn) {
-    // btn.onclick = sayHello;
-    // btn.onmouseenter = function () {
-    //     console.log("You entered");
-    // }
-    btn.addEventListener("dblclick", function () {
-        console.log("you double clicked me");
-    });
-}
+// for (btn of btn) {
+//     btn.onclick = sayHello;
+//     btn.onmouseenter = function () {
+//         console.log("You entered");
+//     }
+//     btn.addEventListener("dblclick", function () {
+//         console.log("you double clicked me");
+//     });
+// }
 
 function sayHello() {
     alert('msg');
@@ -41,7 +41,32 @@ box.addEventListener("mouseenter", function () {
 
 //! this event Listners 
 
-let btns = document.querySelector(".btns");
-btns.addEventListener("click", function () {
-    console.dir(this);
+
+let btns = document.querySelectorAll(".btns"); // Select all elements with class "btns"
+let span = document.querySelector("span");
+let main = document.querySelector("main");
+let section = document.querySelector("section");
+
+function changecolor() {
+    console.dir(this.innerText);
+    this.style.backgroundColor = "pink"; // Corrected property name
+}
+
+// Add event listener to each button
+btns.forEach(btn => {
+    btn.addEventListener("click", changecolor);
 });
+
+// Add event listener to the span
+span.addEventListener("click", changecolor);
+main.addEventListener("click", changecolor);
+section.addEventListener("click", changecolor);
+
+
+//! Keyboard events 
+
+let inp = document.querySelector("input");
+
+inp.addEventListener("keydown", function() {
+    console.log("Key was Pressed");
+})
