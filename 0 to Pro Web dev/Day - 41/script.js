059826d63b11af1f961c1ce74b231f0a15a6163b
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-03-20 08:54:45 +0530

    'update'

--- 0 to Pro Web dev/Day - 41/script.js
+++ 0 to Pro Web dev/Day - 41/script.js
@@ -1,4 +1,4 @@
-h1 = document.querySelector("h1");
+// h1 = document.querySelector("h1");
 
 //! Callbacks Nesting -> Callback Hell
 
@@ -48,48 +48,98 @@ function savetoDb(data, success, failure) {
 // );
 
 
-//! Promises 
+// //! Promises 
 
-function savetoDb(data) {
-    return new Promise((success, failure) => {
-        let internetspeed = Math.floor(Math.random() * 10) + 1;
-        if (internetspeed > 4) {
-            success("Succes : data was saved");
-        } else {
-            failure("failure : Data not saved");
-        }
-    });
+// function savetoDb(data) {
+//     return new Promise((success, failure) => {
+//         let internetspeed = Math.floor(Math.random() * 10) + 1;
+//         if (internetspeed > 4) {
+//             success("Succes : data was saved");
+//         } else {
+//             failure("failure : Data not saved");
+//         }
+//     });
+// }
+
+// savetoDb("Mohit Raj");
+
+// //! Then & Catch 
+
+// let request = savetoDb("Mohit Raj");
+// request.then(() => {
+//     console.log("Promise was resolved");
+// })
+//     .catch(() => {
+//         console.log("Promise was rjected");
+//     })
+
+
+// //!Promises Chaning 
+
+// savetoDb("Hello World")
+//     .then(() => {
+//         console.log("data 1 is saved Now Here");
+//         savetoDb("Hello World")
+//             .then(() => {
+//                 console.log("data 2 Saved");
+//             })
+//     })
+//     .catch(() => {
+//         console.log("Promise was rejceted");
+//     })
+
+
+
+// //! promises 
+// savetoDb("Hello Mohit")
+//     .then((result) => {
+//         console.log("data1 Saved");
+//         console.log(result);
+//         return savetoDb("Hello world");
+//     })
+//     .then((result) => {
+//         console.log("data 2 Saved");
+//         console.log(result);
+//         return savetoDb("Mk Kumar")
+//     })
+//     .then((result) => {
+//         console.log("data 3 saved");
+//         console.log(result);
+//     })
+//     .catch((error) => {
+//         console.log("Promise was rejected");
+//         console.log(error);
+//     })
+
+
+
+h1 = document.querySelector("h1");
+
+// ! Callbacks Nesting -> Callback Hell
+
+function changecolor(color, delay) {
+    return new Promise((resolve, rjecte) => {
+        setTimeout(() => {
+            h1.style.color = color;
+            resolve("color changed")
+        }, delay);
+    })
 }
 
-savetoDb("Mohit Raj");
-
-//! Then & Catch 
-
-let request = savetoDb("Mohit Raj");
-request.then(() => {
-    console.log("Promise was resolved");
-})
-    .catch(() => {
-        console.log("Promise was rjected");
-    })
-
-
-//!Promises Chaning 
-
-savetoDb("Hello World")
+changecolor("red", 1000)
     .then(() => {
-        console.log("data 1 is saved Now Here");
-        savetoDb("Hello World")
-            .then(() => {
-                console.log("data 2 Saved");
-            })
+        console.log("red Color was changed");
+        return changecolor("orange", 1000);
     })
-    .catch(() => {
-        console.log("Promise was rejceted");
+    .then(() => {
+        console.log("Orange color was completed");
+        return changecolor("green", 1000);
+    })
+    .then(() => {
+        console.log("green color was compledted");
+        return changecolor("blue", 1000);
+    })
+    .then(() => {
+        console.log("Blue color was changed");
     })
 
-
-
-    //! promises 
-
-    
