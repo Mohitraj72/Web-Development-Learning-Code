955ca358ec30028582a4faafb5a7c27729ad8742
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-03-26 07:56:56 +0530

    'update'

--- 0 BACKEND/Folder - 2/index.js
+++ 0 BACKEND/Folder - 2/index.js
@@ -10,7 +10,40 @@ app.listen(port, () => {
 });
 
 
-app.use((req, res) => {
-    console.log(req);
-    console.log("Request recievd");
+// app.use((req, res) => {
+//      console.log(req);
+//     console.log("Request recievd");
+//     let code = "<h1> Fruits is here for you </h1>";
+//     res.send(code);
+// });
+
+
+app.get("/", (req, res) => {
+    res.send("you contacted root path by nodemon ");
+})
+
+// app.get("/apple", (req, res) => {
+//     res.send("you contated apple path");
+// })
+
+// app.get("/home", (req, res) => {
+//     res.send("you contated home path");
+// })
+
+// app.get("*", (req, res) => {
+//     res.redirect("/");
+// }) 
+
+app.get("/:username/:id", (req, res) => {
+    let { username, id } = req.params;
+    let html = `<h1> Welcome to the here @${username}</h1>`
+    res.send(html);
+});
+
+app.get("/search", (req, res) => {
+    let { q } = req.query;
+    if(!q) {
+        res.send("nothing Search")
+    }
+    res.send(`search result for query${q}`)
 });
