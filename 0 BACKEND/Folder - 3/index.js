ef1b9669fdef5eafe04b59ee8e2a509c100598b4
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-03-26 11:51:28 +0530

    'update'

--- 0 BACKEND/Folder - 3/index.js
+++ 0 BACKEND/Folder - 3/index.js
@@ -1,31 +1,43 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const instadata = require("./data.json");
 
 const port = 8080;
 
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.render("home.ejs")
 });
 
 
-app.get("/ig/:username",(req,res) => {
-    let {username} = req.params;
+app.get("/ig/:username", (req, res) => {
+    // const followers = ["raj", "sahil", "Mohit", "raushan"];
+    // let {username} = req.params;
     // console.log(username);
-    res.render("instagram.ejs", {username})
-} );
+    // console.log(instadata);
+
+    let { username } = req.params;
+    const data = instadata[username];
+    // console.log(data);
+    if (data) {
+        res.render("instagram.ejs", { data });
+    } else {
+        res.render("error.ejs");
+    }
+});
 
 
-app.get("/hello", (req,res) => {
+app.get("/hello", (req, res) => {
     res.send("Hello Guys ")
 });
 
-app.get("/roll", (req,res) => {
-    let diceval = Math.floor(Math.random()*6) + 1;
-    res.render("roll.ejs", {diceval});
+app.get("/roll", (req, res) => {
+    let diceval = Math.floor(Math.random() * 6) + 1;
+    res.render("roll.ejs", { diceval });
 });
 
 app.listen(port, () => {
