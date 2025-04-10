ab4ca489ab0d25e800fed39841871de33187f4d8
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-04-10 07:45:07 +0530

    'update'

---
+++ 0 to Pro Web dev/Reacts-Shreyians/my-project/src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="w-full h-screen bg-zinc-200">
+      <div className="px-3 py-4 bg-zinc-400 absolute left-1/2 top-1/2 -translate-x-[50%] translate-y-[50%] rounded-md ">
+        <div className="w-32 h-32 bg-zinc-300">  </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
