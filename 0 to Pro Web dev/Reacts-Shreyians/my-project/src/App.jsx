c36fd86ec5eaee29fabe02c079dafa4f87ffcad5
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-04-29 14:43:06 +0530

    'updata'

--- 0 to Pro Web dev/Reacts-Shreyians/my-project/src/App.jsx
+++ 0 to Pro Web dev/Reacts-Shreyians/my-project/src/App.jsx
@@ -4,16 +4,16 @@ import Card from './Components/Card';
 
 function App() {
 
-  const data = ["harsh", "nsihu", "someone else"]
-
+  
   
   return (
     <div>
-      <Card/>
-      {data.map(() => "Hello" )}
-      </div>
+      
+      
+    </div>
+    
   );
 }
-asddadasdsdassasadqwqe
+
 
 export default App;
