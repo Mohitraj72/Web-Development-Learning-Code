5641ab851a218738a62ba17b805838734df8cdb2
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-05-02 21:37:29 +0530

    'update'

--- 0 to Pro Web dev/Reacts-Shreyians/my-project/src/App.jsx
+++ 0 to Pro Web dev/Reacts-Shreyians/my-project/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from './Components/Card';
 import Song from './Components/Song';
+import State from './Components/State';
 
 
 function App() {
@@ -9,8 +10,7 @@ function App() {
   
   return (
     <div>
-      <Card/>
-      <Song/>
+     <State/>
     </div>
     
   );
--- 0 to Pro Web dev/Reacts-Shreyians/my-project/src/Components/State.jsx
+++ 0 to Pro Web dev/Reacts-Shreyians/my-project/src/Components/State.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
 
 function State() {
+  const [score, setScore] = useState(100);
 
-  useState ()
+  
+
+  
     
   return (
-    <div>State</div>
+    <div className='p-4'>
+      <h1>{score}</h1>
+      <button onClick={() => setScore(2232)} className='px-2 py-1 text-xs bg-blue-500 text-white rounded-full'> Change </button>
+    </div>
   )
 }
 
