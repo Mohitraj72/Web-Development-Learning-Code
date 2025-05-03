c46447ff56f26c814b8b596f4f0a183806fb1954
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-05-03 13:04:58 +0530

    'update'

--- 0 to Pro Web dev/Reacts-Shreyians/my-project/src/Components/State.jsx
+++ 0 to Pro Web dev/Reacts-Shreyians/my-project/src/Components/State.jsx
@@ -1,18 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function State() {
   const [score, setScore] = useState(100);
 
-  
-
-  
-    
   return (
-    <div className='p-4'>
+    <div className="p-4">
       <h1>{score}</h1>
-      <button onClick={() => setScore(2232)} className='px-2 py-1 text-xs bg-blue-500 text-white rounded-full'> Change </button>
+      <button
+        onClick={() => setScore(2232)}
+        className="px-2 py-1 text-xs bg-blue-500 text-white rounded-full"
+      > Change </button>
     </div>
-  )
+  );
 }
 
-export default State;   asfssddrty
+export default State;
