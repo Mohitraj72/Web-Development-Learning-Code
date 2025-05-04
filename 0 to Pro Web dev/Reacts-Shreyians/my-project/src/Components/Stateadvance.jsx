f625ba4de66f5b9c921e34cb289b9d6bcad741ab
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-05-04 07:08:03 +0530

    'update'

--- 0 to Pro Web dev/Reacts-Shreyians/my-project/src/Components/Stateadvance.jsx
+++ 0 to Pro Web dev/Reacts-Shreyians/my-project/src/Components/Stateadvance.jsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function Stateadvance() {
-  const [val, setVal] = useState({name: "Raj", isBanned: false});
-
+  const [val, setVal] = useState({ name: "Raj", isBanned: false });
 
   return (
-    <div className='py-4'>
-        <h1>name: {val.name}</h1>
-        <h2>banned: {val.isBanned.toString()}</h2>
-        <button  onClick={()=> setVal({...val, isBanned: !val.isBanned})} className='px-3 py-1 bg-blue-500 rounded-full text-white '>Change </button>
+    <div className="py-4">
+      <h1>name: {val.name}</h1>
+      <h2>banned: {val.isBanned.toString()}</h2>
+      <button
+        onClick={() => setVal({ ...val, isBanned: !val.isBanned })}
+        className={`px-3 py-1 ${
+          !val.isBanned ? "bg-blue-500" : "bg-red-500"
+        } rounded-full text-white `}
+        >
+        Change
+      </button>
     </div>
-  )
+  );
 }
 
 export default Stateadvance;
