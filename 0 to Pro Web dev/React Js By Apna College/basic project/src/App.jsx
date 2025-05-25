a73073d0df8d3268b7ef8278d2c92f3c9e5230ba
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-05-25 10:55:57 +0530

    'update'

--- 0 to Pro Web dev/React Js By Apna College/basic project/src/App.jsx
+++ 0 to Pro Web dev/React Js By Apna College/basic project/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Title from "./Title";
 
 function App() {
-  return <button>Hello </button>;
+  return (
+    <>
+      <Title />
+      <button>Hello </button>
+      hello this is Your Nutton to make the clicky
+    </>
+  );
 }
 
 export default App;
---
+++ 0 to Pro Web dev/React Js By Apna College/basic project/src/Title.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Title() {
+  return (
+    <div> I am the Title Here </div>
+  )
+}
+
+export default Title;
