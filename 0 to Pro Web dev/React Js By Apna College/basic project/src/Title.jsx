59258497689a9a6b37412e991dc06946e8909476
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-05-25 11:10:37 +0530

    'update'

--- 0 to Pro Web dev/React Js By Apna College/basic project/src/Title.jsx
+++ 0 to Pro Web dev/React Js By Apna College/basic project/src/Title.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function Title() {
+    const name = "Mohit";
+
   return (
-    <div> I am the Title Here </div>
-  )
+    <>
+      <p>2 * 2 = {2 * 2}</p>
+      <p>hi, {name.toUpperCase()} </p>
+      <div> I am the Title Here </div>
+      <p>2 </p>
+    </>
+  );
 }
 
 export default Title;
