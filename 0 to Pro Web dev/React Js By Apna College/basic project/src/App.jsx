a4c149c6baacc38d60a192cbfae12e4c42b6d0ef
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-05-25 11:25:03 +0530

    'update'

--- 0 to Pro Web dev/React Js By Apna College/basic project/src/App.jsx
+++ 0 to Pro Web dev/React Js By Apna College/basic project/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import Title from "./Title";
+import Product from "./Product";
+import ProductTab from "./ProductTab";
 
 function App() {
   return (
     <>
+      <ProductTab />
       <Title />
       <button>Hello </button>
       hello this is Your Nutton to make the clicky
---
+++ 0 to Pro Web dev/React Js By Apna College/basic project/src/Product.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Product.css";
+
+function Product() {
+  return (
+    <>
+      <div className="Product">
+        <h5>Title </h5>
+      <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+    </>
+  );
+}
+
+export default Product;
