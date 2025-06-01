350792f48688cf834710e33794b8911a62d60711
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2025-06-01 20:42:11 +0530

    'update'

--- 0 to Pro Web dev/React Js By Apna College/Study ch 6/src/App.jsx
+++ 0 to Pro Web dev/React Js By Apna College/Study ch 6/src/App.jsx
@@ -1,18 +1,15 @@
-import './App.css'
+import "./App.css";
 import Lottery from "./Lottery";
-import Ticket from './Ticket';
+import Ticket from "./Ticket";
 
 function App() {
-  
-
   return (
     <>
-     
       {/* <Lottery/> */}
-      <Ticket ticket = {[0,1,2]}/>
-      <h2>Helo </h2>
+      {/* <Ticket ticket = {[0,1,2]}/> */}
+      <Lottery n={3} winningSum={15} />
     </>
-  )
+  );
 }
 
 export default App;
